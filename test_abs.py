c0e54cef68be608c5895d9c1c0c287bc9635a888
Fam A. Shihata <fam@awadlouis.com>
2026-09-17 13:42:11 +0200

    feat: add abs

---
+++ test_abs.py
@@ -0,0 +1,12 @@
+import pytest 
+
+from abs import abs
+
+
+@pytest.mark.parametrize("a, expected", [
+    (3, 3),
+    (0, 0),
+    (-1, 1),
+])
+def test_abs(a, expected):
+    assert abs(a) == expected
