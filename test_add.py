eb194cb0761893207b54c4b49e5353a1862e87c2
Fam A. Shihata <fam.shihata@sap.com>
2026-09-17 11:06:02 +0200

    feat: add addition

---
+++ test_add.py
@@ -0,0 +1,10 @@
+import pytest
+from add import addition
+
+@pytest.mark.parametrize("a, b, expected", [
+    (1, 2, 3),
+    (0, 0, 0),
+    (-1, 1, 0),
+])
+def test_addition(a, b, expected):
+    assert addition(a, b) == expected
